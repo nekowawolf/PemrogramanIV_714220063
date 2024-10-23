195073a286f7d8d0054ea07f339b69676bdbc7ca
nekowawolf <irukainu20@gmail.com>
2024-10-24 06:04:07 +0700

    add: Pertemuan03

---
+++ Pertemuan03/Praktikum/flutter_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/material_app.dart';
+
+void main() {
+  runApp(const AppMaterial());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('ULBI pls'),
+        ),
+      ),
+    );
+  }
+}
